e9ca1d439253e2fbc01c882e7091eaf15700878f
Mr. Mendez <mrmendez@amvestor.com>
2025-01-19 13:23:43 -0500

    Read Daemon Surrender

---
+++ test.svelte
@@ -0,0 +1,23 @@
+<script>
+	let items = ["item 1"]
+	
+	export function addItem() {
+		console.log("working")
+		const lastItem = items[items.length -1]
+		const lastItemId =  parseInt(lastItem.match(/\d+/)[0])
+
+		const newItem = `item ${lastItemId + 1}`
+
+		items.push(newItem)
+	}
+	
+</script>
+<ul>
+{#each items as item}
+	<li>
+		{item}
+	</li>
+{/each}
+</ul>
+
+<button on:click={addItem}> Add Item </button>
